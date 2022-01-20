13dfc11c7b900351eb39a165e70f7c45cf606a60
Pavel Levchenko <plevchen@student.ubc.ca>
2022-01-19 22:03:12 -0800

    initial commit update

---
+++ R/Rstockwatch.R
@@ -0,0 +1,23 @@
+#' Calculates daily percentage change of a stock price within a given period of time
+
+percent_change <- function(stock_ticker, start_date, end_date){
+	print('TODO')
+}
+
+
+#' Visualizes trend of a stock price change against the market benchmark within a given period of time
+profit_viz <- function(stock_ticker, start_date, end_date, benchmark_ticker){
+	print('TODO')
+}
+
+
+#' Calculates the daily trading volume change status of a stock within a given period of time
+volume_change <- function(stock_ticker, start_date, end_date){
+	print('TODO')
+}
+
+
+#' Visualizes the daily trading volume of a stock using bar plot within a given period of time
+volume_viz <- function(stock_ticker, start_date, end_date){
+	print('TODO')
+}
