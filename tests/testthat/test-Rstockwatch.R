b125238a05f409264e543b19c89d38f175e199a8
Pavel Levchenko <plevchen@student.ubc.ca>
2022-01-27 16:06:05 -0800

    added tests for volume_viz function

--- tests/testthat/test-Rstockwatch.R
+++ tests/testthat/test-Rstockwatch.R
@@ -1,3 +1,16 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+
+
+
+
+
+
+test_that("volume_viz is correct", {
+  vol_plot <- volume_viz('AAPL', '2017-01-01', '2017-01-10')         # sample plot with a few data
+  
+  expect_true("GeomBar" %in% c(class(vol_plot$layers[[1]]$geom)))    # Volumevisualization should be bar chart
+  expect_true("date"  == rlang::get_expr(vol_plot$mapping$x))        # X axis should correspond to "date"
+  expect_true("volume" == rlang::get_expr(vol_plot$mapping$y))       # Y axis should correspond to "volume"
+  expect_true("Price_change"  == rlang::get_expr(vol_plot$mapping$fill))   # Bars should be colored by "Price change"
+  expect_true("Price_change" == rlang::get_expr(vol_plot$mapping$colour))  # Bars should be colored by "Price change"
+  
 })
