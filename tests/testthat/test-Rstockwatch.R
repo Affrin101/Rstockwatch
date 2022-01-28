7a717c7eafedcda0034ae3377af8a92d603ad0be
Pavel Levchenko <plevchen@student.ubc.ca>
2022-01-28 13:39:56 -0800

    added tests for input parameters of dataframe

--- tests/testthat/test-Rstockwatch.R
+++ tests/testthat/test-Rstockwatch.R
@@ -5,8 +5,13 @@
 
 
 test_that("volume_viz is correct", {
+  vdf <- volume_change('AAPL', '2017-01-01', '2017-01-10')  #Initialize sample of input data based on another function
+  expect_true(is.data.frame(vdf))  # Input data should be dataframe
+  expect_true("date" %in% colnames(dfout))
+  expect_true("volume" %in% colnames(dfout))
+  expect_true("Price_change" %in% colnames(dfout))
+
   vol_plot <- volume_viz('AAPL', '2017-01-01', '2017-01-10')         # sample plot with a few data
-  
   expect_true("GeomBar" %in% c(class(vol_plot$layers[[1]]$geom)))    # Volumevisualization should be bar chart
   expect_true("date"  == rlang::get_expr(vol_plot$mapping$x))        # X axis should correspond to "date"
   expect_true("volume" == rlang::get_expr(vol_plot$mapping$y))       # Y axis should correspond to "volume"
