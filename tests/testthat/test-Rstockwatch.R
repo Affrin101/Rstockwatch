8bd141a0c240768ff0ef8a67e56e5a6a85c740b7
Helin Wang <helena.wang.2021@gmail.com>
2022-01-28 21:50:11 -0800

    add tests for input

--- tests/testthat/test-Rstockwatch.R
+++ tests/testthat/test-Rstockwatch.R
@@ -1,3 +1,14 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+# Test input stocke ticker
+test_that("Error of stock_ticker", {
+  expect_error(percent_change("ABCD", "2017-01-01", "2017-01-10"))
+})
+
+# Test start_date format
+test_that("Error of start_date format", {
+  expect_error(percent_change("AAPL", "20170101", "2017-01-10"))
+})
+
+# Test end_date format
+test_that("Error of end_date format", {
+  expect_error(percent_change("AAPL", "2017-01-01", "2017/01/10"))
 })
