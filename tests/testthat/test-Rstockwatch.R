fdf5ba640538da383c18a078724aca5790e95b6d
sy25wang <90230188+sy25wang@users.noreply.github.com>
2022-02-04 01:07:44 -0800

    Update test suites

--- tests/testthat/test-Rstockwatch.R
+++ tests/testthat/test-Rstockwatch.R
@@ -1,5 +1,8 @@
-
 # Test for percent change function
+# Test input stock ticker
+test_that("Error of stock_ticker", {
+  expect_error(percent_change("ABCD", "2017-01-01", "2017-01-10"))
+})
 
 # Test start_date format
 test_that("Error of start_date format", {
@@ -55,11 +58,4 @@ test_that("volume_viz is correct", {
   expect_true("date" %in% colnames(vdf))
   expect_true("volume" %in% colnames(vdf))
   expect_true("Price_change" %in% colnames(vdf))
-  
-  vol_plot <- volume_viz('AAPL', '2017-01-01', '2017-01-10')         # sample plot with a few data
-  expect_true("GeomBar" %in% c(class(vol_plot$layers[[1]]$geom)))    # Volumevisualization should be bar chart
-  expect_true("date"  == rlang::get_expr(vol_plot$mapping$x))        # X axis should correspond to "date"
-  expect_true("volume" == rlang::get_expr(vol_plot$mapping$y))       # Y axis should correspond to "volume"
-  expect_true("Price_change"  == rlang::get_expr(vol_plot$mapping$fill))   # Bars should be colored by "Price change"
-  expect_true("Price_change" == rlang::get_expr(vol_plot$mapping$colour))  # Bars should be colored by "Price change"
 })
